f84fc40c84acd50e13177fd06d17d2912d342e79
Ryan Sorensen <ry.sorensen@gmail.com>
2024-02-12 11:45:02 -0700

    test

--- system/startup.sh
+++ system/startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# echo "Copying custom default.conf over to /etc/nginx/sites-available/default.conf"
+echo "Copying custom default.conf over to /etc/nginx/sites-available/default.conf"
 
-# NGINX_CONF=/home/site/wwwroot/default.conf
+NGINX_CONF=/home/site/wwwroot/default.conf
 
-# if [ -f "$NGINX_CONF" ]; then
-#     cp /home/site/wwwroot/default.conf /etc/nginx/sites-available/default
-# echo "restart nginx"
-# service nginx reload
-# else
-#     echo "File does not exist, skipping cp."
-# fi
+if [ -f "$NGINX_CONF" ]; then
+    cp /home/site/wwwroot/default.conf /etc/nginx/sites-available/default
+echo "restart nginx"
+service nginx reload
+else
+    echo "File does not exist, skipping cp."
+fi
